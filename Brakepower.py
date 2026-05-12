8453ce0495a4a1133efaa38fd42ec379dfc8c520
sharxths <sharathsaunshi0@gmail.com>
2026-05-12 23:41:01 +0530

    Add BSFC calculation to Brake Power Calculator

--- Brakepower.py
+++ Brakepower.py
@@ -1,10 +1,13 @@
 import math
 
-print("\n Brake Power Calculator \n")
+print("\n Brake Power and BSFC Calculator \n")
 
 N = float(input("Enter the speed of engine in rpm : "))
 T = float(input("Enter the torque in Nm : "))
 
 BP = (2*math.pi*N*T)/(60000) #in KW.
 
-print(f"{BP:.3f} KW")
+Mf = float(input("Enter mass flow rate of fuel in kg/s : "))
+BSFC = Mf/BP 
+print(f"Brake power of engine is: {BP:.3f} KW")
+print(f"BSFC of engine is: {BSFC:.3f} kg/kWh")
